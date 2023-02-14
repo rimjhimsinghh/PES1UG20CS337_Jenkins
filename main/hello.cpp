aa10e4875db95dabd062aad67e026a7fcf32000f
Rimjhim Singh <singhrimjhim2809@gmail.com>
2023-02-15 02:07:39 +0530

    Webhooks

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "NAME: Rimjhim Singh\nSRN: PES1UG20CS337";
+  cout << "NAME: Rimjhim Singh\nSRN: PES1UG20CS337\n";
+  cout << "I'm trying out Webhooks which is totally cool!\n";
   return 0;
 }
