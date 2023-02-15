fb1b6d7e9030c7e79984d90bd93b617f790bcacf
Rimjhim Singh <72965011+rimjhimsinghh@users.noreply.github.com>
2023-02-15 21:27:14 +0530

    Create hello_337.cpp

---
+++ hello_337.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "NAME: Rimjhim Singh\nSRN: PES1UG20CS337\n";
+  cout << "I'm trying out Pipeline\n";
+  return 0;
+}
